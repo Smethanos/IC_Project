bfab6f75ad42cea1c16c8961a3d14a6c3868128f
dipicarra <icldiogo@gmail.com>
2023-10-31 14:27:54 +0000

    profiles updtade

---
+++ SmartHome/scripts/profile.js
@@ -0,0 +1,16 @@
+"use strict"
+
+function onButtonClick() {
+    alert('Button clicked!');
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', onButtonClick);
+
+const newButton = document.createElement('button');
+newButton.textContent = 'Click me!';
+document.body.appendChild(newButton);
+
+newButton.addEventListener('click', () => {
+    alert('New button clicked!');
+});
